defac0087fc1b62d9a9fea6a36bda3156405dc5c
Tom Marshall <tdm@cyngn.com>
2014-09-17 23:07:00 +0200

    camera: put back USE_DEVICE_SPECIFIC_CAMERA
    Authored-by: Iliyan Malchev <malchev@google.com>
    Change-Id: I7b992e5bad7bb807fcdb379553b52f653a1c245b
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- Android.mk
+++ Android.mk
@@ -19,9 +19,11 @@ MM_V4L2_DRIVER_LIST += msmcobalt
 MM_V4L2_DRIVER_LIST += msmfalcon
 
 ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
+ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       include $(call all-subdir-makefiles)
     endif
   endif
+endif
 endif
